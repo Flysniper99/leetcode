3dfc45e7d99ebca9c1222cc528460b4fd284a6b0
flysniper <473913850@qq.com>
2023-12-12 23:49:05 +0800

    KMP算法题

---
+++ src/Solution_28.java
@@ -0,0 +1,30 @@
+class Solution_28 {
+    public int strStr(String haystack, String needle) {
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) j++;
+            if (j == needle.length()) {
+                return i + 1 - needle.length();
+            }
+        }
+        return -1;
+
+    }
+
+    private void getNext(int[] next, String s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i)) {
+                j = next[j - 1];
+            }
+            if (s.charAt(j) == s.charAt(i)) j++;
+            next[i] = j;
+        }
+    }
+}
