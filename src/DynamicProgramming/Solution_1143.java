f5154ce89fc3c6756f6bfd13b061612a5f0bc899
flysniper <473913850@qq.com>
2024-02-27 10:29:51 +0800

    2024.2.26

--- src/DynamicProgramming/Solution_1143.java
+++ src/DynamicProgramming/Solution_1143.java
@@ -10,35 +10,37 @@ class Solution_1143 {
         char[] txt1 = text1.toCharArray();
         char[] txt2 = text2.toCharArray();
         int[][] dp = new int[txt1.length][txt2.length];
-//        for (int i = 0; i <= txt1.length - 1; i++) {
-//            if (txt1[i] == txt2[0]) {
-//                for (int j = i; ; j++) {
-//                    dp[j][0] = 1;
-//                    if (j == txt1.length - 1) break;
-//                }
-//            }
-//        }
-//        for (int i = 0; i <= txt2.length - 1; i++) {
-//            if (txt2[i] == txt1[0]) {
-//                for (int j = i; ; j++) {
-//                    dp[0][j] = 1;
-//                    if (j == txt2.length - 1) break;
-//                }
-//            }
-//        }
         for (int i = 0; i <= txt1.length - 1; i++) {
-            dp[i][0] = 1;
+            if (txt1[i] == txt2[0]) {
+                for (int j = i; ; j++) {
+                    dp[j][0] = 1;
+                    if (j == txt1.length - 1) break;
+                }
+                break;
+            }
         }
-        for (int i = 0; i <= txt1.length - 1 && txt1[i] != txt2[0]; i++) {
-            dp[i][0] = 0;
+        for (int i = 0; i <= txt2.length - 1; i++) {
+            if (txt2[i] == txt1[0]) {
+                for (int j = i; ; j++) {
+                    dp[0][j] = 1;
+                    if (j == txt2.length - 1) break;
+                }
+                break;
+            }
         }
-
-        for (int j = 0; j <= txt2.length - 1; j++) {
-            dp[0][j] = 1;
-        }
-        for (int j = 0; j <= txt2.length - 1 && txt2[j] != txt1[0]; j++) {
-            dp[0][j] = 0;
-        }
+//        for (int i = 0; i <= txt1.length - 1; i++) {
+//            dp[i][0] = 1;
+//        }
+//        for (int i = 0; i <= txt1.length - 1 && txt1[i] != txt2[0]; i++) {
+//            dp[i][0] = 0;
+//        }
+//
+//        for (int j = 0; j <= txt2.length - 1; j++) {
+//            dp[0][j] = 1;
+//        }
+//        for (int j = 0; j <= txt2.length - 1 && txt2[j] != txt1[0]; j++) {
+//            dp[0][j] = 0;
+//        }
 
         for (int i = 1; i <= txt1.length - 1; i++) {
             for (int j = 1; j <= txt2.length - 1; j++) {
@@ -50,5 +52,22 @@ class Solution_1143 {
             }
         }
         return dp[txt1.length - 1][txt2.length - 1];
+    }
+
+    public int longestCommonSubsequence_2(String text1, String text2) {
+        char[] txt1 = text1.toCharArray();
+        char[] txt2 = text2.toCharArray();
+        int[][] dp = new int[txt1.length + 1][txt2.length + 1];
+
+        for (int i = 1; i <= txt1.length; i++) {
+            for (int j = 1; j <= txt2.length; j++) {
+                if (txt1[i - 1] == txt2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[txt1.length][txt2.length];
     }
 }
--- src/DynamicProgramming/Solution_718.java
+++ src/DynamicProgramming/Solution_718.java
@@ -28,4 +28,19 @@ class Solution_718 {
         }
         return res;
     }
+
+    public int findLength_2(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int res = 0;
+
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+        return res;
+    }
 }
