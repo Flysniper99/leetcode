dbe3b3a9ae8dd11bfe0ccd8945a212a6fd2dd0df
flysniper <473913850@qq.com>
2024-03-11 15:19:00 +0800

    2024.3.11

--- src/DynamicProgramming/Solution_5.java
+++ src/DynamicProgramming/Solution_5.java
@@ -2,7 +2,7 @@ package DynamicProgramming;
 
 /**
  * 最长回文子串
- * p[i][j]：表示区间范围[i,j] （注意是左闭右闭）的子串是否是回文子串，
+ * dp[i][j]：表示区间范围[i,j] （注意是左闭右闭）的子串是否是回文子串，
  * 如果是dp[i][j]为true，否则为false。
  * 是回文子串的话，与之前记录的最大回文子串长度进行对比以更新结果
  */
@@ -12,7 +12,6 @@ class Solution_5 {
         boolean[][] dp = new boolean[chs.length][chs.length];
         int temp = 0;
         String res = s.substring(0,1);
-
 
         for (int i = chs.length - 1; i >= 0; i--) {
             for (int j = i; j <= chs.length - 1; j++) {
