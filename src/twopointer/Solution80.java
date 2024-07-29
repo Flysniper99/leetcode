1386a293568a8e7cadbeafb1d6154319991cfd8d
flysniper <473913850@qq.com>
2024-07-30 00:15:28 +0800

    2024.7.30

--- src/twopointer/Solution80.java
+++ src/twopointer/Solution80.java
@@ -5,13 +5,21 @@
 
 package twopointer;
 
+import java.util.Arrays;
+
 /**
  * @author : f50040149
  * @date : 2024/7/29 17:38
  * @since : 1.0
  */
 public class Solution80 {
-    public int removeDuplicates(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,2,2,3};
+        int res = removeDuplicates(nums);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(res);
+    }
+    public static int removeDuplicates(int[] nums) {
         int n = nums.length;
         if (n == 1) {
             return n;
@@ -24,6 +32,9 @@ public class Solution80 {
         for (int i = 1; i < n; i++) {
             if (nums[i] == nums[i - 1]) {
                 cnt++;
+                if (i == n - 1) {
+                    index = index - 1;
+                }
                 if (cnt == 2) {
                     index = i + 1;
                 }
@@ -32,7 +43,12 @@ public class Solution80 {
                     for (int j = 0; j < n - i; j++) {
                         nums[index + j] = nums[i + j];
                     }
+                    n = n - (i - index);
+//                    for (int j = n - 1; j > n - i + index - 1; j--) {
+//                        nums[j] = 0;
+//                    }
                     cnt = 1;
+                    i = index;
                 } else if (cnt < 2) {
                     index++;
                 } else {
@@ -40,6 +56,6 @@ public class Solution80 {
                 }
             }
         }
-        return index;
+        return index + 1;
     }
 }
