eeab9d9349601f772ce778519a3d0c7910d93301
f50040149 <fangjian24@huawei.com>
2024-07-29 15:54:02 +0800

    2024.7.29

--- src/twopointer/Solution75.java
+++ src/twopointer/Solution75.java
@@ -5,28 +5,40 @@
 
 package twopointer;
 
+import java.util.Arrays;
+
 /**
  * @author : f50040149
  * @date : 2024/7/26 20:18
  * @since : 1.0
  */
 class Solution75 {
-    public void sortColors(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 0, 1};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    //快慢指针，两个for，一个for把0全部移到左边，一个for把2全部移到右边
+    public static void sortColors(int[] nums) {
         int n = nums.length;
         int left = 0;
         int right = n - 1;
-        while (left < right) {
-            if (nums[left] > nums[right]) {
-                swap(nums, left, right);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left, i);
                 left++;
+            }
+        }
+        for (int i = n - 1; i >= left; i--) {
+            if (nums[i] == 2) {
+                swap(nums, right, i);
                 right--;
-            } else if (nums[left] < nums[right]) {
-                left++;
             }
         }
     }
 
-    public void swap(int[] nums, int l, int r) {
+    public static void swap(int[] nums, int l, int r) {
         int temp = nums[l];
         nums[l] = nums[r];
         nums[r] = temp;
