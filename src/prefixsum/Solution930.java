e9f7c4cd274609c3f5d79d31a6ca11749db2d643
f50040149 <fangjian24@huawei.com>
2024-08-13 08:56:26 +0800

    2024.8.12

--- src/prefixsum/Solution930.java
+++ src/prefixsum/Solution930.java
@@ -19,6 +19,7 @@ class Solution930 {
         int preSum = 0;
         int res = 0;
         for (int i : nums) {
+            //先put再计算preSum，先put是因为preSum中会出现==goal的情况，最后一个preSum不需要加入map中
             map.put(preSum, map.getOrDefault(preSum, 0) + 1);
             preSum += i;
             res += map.getOrDefault(preSum - goal, 0);
